28222eb9c06762bf0a9df18e47fd317be87a0712
geffreyw <geffreyw@hotmail.com>
2018-12-01 11:36:31 +0100

    login/reg opt met hash + styling

--- src/main/java/be/thomasmore/travelmore/controlller/AuthController.java
+++ src/main/java/be/thomasmore/travelmore/controlller/AuthController.java
@@ -15,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionContext;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Enumeration;
 import java.util.List;
 
@@ -55,7 +57,7 @@ public class AuthController {
     }
 
     public String aanmelden() {
-        List<Persoon> ingelogdePersoon = persoonService.authPersoon(newPersoon.getEmail(), newPersoon.getWachtwoord());
+        List<Persoon> ingelogdePersoon = persoonService.authPersoon(newPersoon.getEmail(), this.hashPassword(newPersoon.getWachtwoord()));
 
         if(ingelogdePersoon.size() > 0){
             logedinPersoon = ingelogdePersoon.get(0);
@@ -75,6 +77,7 @@ public class AuthController {
 
     public String registreren(){
         if(this.persoonService.getByMail(newPersoon.getEmail()).size() == 0){
+            newPersoon.setWachtwoord(this.hashPassword(newPersoon.getWachtwoord()));
             this.persoonService.insert(newPersoon);
             newPersoon = new Persoon();
             errorMsg = "";
@@ -85,5 +88,32 @@ public class AuthController {
         }
     }
 
-    //TODO: Wachtwoord vegeten, Wachtwoord hashen
+    private String hashPassword(String password){
+        String passwordToHash = password;
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(passwordToHash.getBytes());
+            //Get the hash's bytes
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    //TODO: Wachtwoord vegeten
 }
